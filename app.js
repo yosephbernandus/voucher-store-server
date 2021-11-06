7b08ba5782fa19e545c1b3bb90971ddd6d46b866
yosephbernandus <yosephbernandus@gmail.com>
2021-11-06 18:27:57 +0700

    add category page table in admin lte

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var dashboardRouter = require('./app/dashboard/router');
 var catetgoryRouter = require('./app/category/router');
 
 var app = express();
@@ -20,7 +21,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/adminlte', express.static(path.join(__dirname, '/node_modules/admin-lte')));
 
 
-app.use('/', catetgoryRouter);
+app.use('/', dashboardRouter);
+app.use('/category', catetgoryRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
