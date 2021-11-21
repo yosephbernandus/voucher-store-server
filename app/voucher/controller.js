7114b15dc2084668b64f6e23f98a3ce2866cb179
yosephbernandus <yosephbernandus@gmail.com>
2021-11-21 13:45:19 +0700

    add edit status button complete

--- app/voucher/controller.js
+++ app/voucher/controller.js
@@ -191,5 +191,26 @@ module.exports = {
             req.flash('alertStatus', 'danger');
             res.redirect('/voucher');
         }
+    },
+
+    actionStatus: async (req, res) => {
+        try {
+            const { id } = req.params;
+            let voucher = await Voucher.findOne({_id: id});
+            let status = voucher.status === 'Y' ? 'N' : 'Y';
+
+           voucher = await Voucher.findOneAndUpdate({
+                _id: id
+            }, {status})
+
+            req.flash('alertMessage', 'Success ubah status voucher');
+            req.flash('alertStatus', 'success');
+            res.redirect('/voucher')
+            
+        } catch (err) {
+            req.flash('alertMessage', `${err.message}`);
+            req.flash('alertStatus', 'danger');
+            res.redirect('/voucher');
+        }
     }
 }
--- app/voucher/router.js
+++ app/voucher/router.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var router = express.Router();
-const { index, viewCreate, actionCreate, viewEdit, actionEdit, actionDelete } = require('./controller');
+const { index, viewCreate, actionCreate, viewEdit, actionEdit, actionDelete, actionStatus } = require('./controller');
 const multer = require('multer');
 const os = require('os');
 
@@ -12,6 +12,7 @@ router.get('/create', viewCreate);
 router.post('/create', multer({dest: os.tmpdir()}).single('image'), actionCreate);
 router.get('/edit/:id', viewEdit);
 router.put('/edit/:id', multer({dest: os.tmpdir()}).single('image'), actionEdit);
+router.put('/status/:id', actionStatus);
 router.delete('/delete/:id', actionDelete);
 
 
