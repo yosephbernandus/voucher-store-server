180e59cc335c727da23308c2ac8a6c63a4260fa7
yosephbernandus <yosephbernandus@gmail.com>
2021-11-27 07:39:23 +0700

    delete payment

--- app/payment/controller.js
+++ app/payment/controller.js
@@ -80,19 +80,19 @@ module.exports = {
         }
     },
 
-    // actionDelete: async (req, res) => {
-    //     try {
-    //         const { id } = req.params;
-    //         await Nominal.findOneAndRemove({_id: id});
+    actionDelete: async (req, res) => {
+        try {
+            const { id } = req.params;
+            await Payment.findOneAndRemove({_id: id});
 
-    //         req.flash('alertMessage', 'Success hapus Nominal');
-    //         req.flash('alertStatus', 'success');
+            req.flash('alertMessage', 'Success hapus Nominal');
+            req.flash('alertStatus', 'success');
 
-    //         res.redirect('/nominal')
-    //     } catch (err) {
-    //         req.flash('alertMessage', `${err.message}`);
-    //         req.flash('alertStatus', 'danger');
-    //         res.redirect('/nominal');
-    //     }
-    // }
+            res.redirect('/payment')
+        } catch (err) {
+            req.flash('alertMessage', `${err.message}`);
+            req.flash('alertStatus', 'danger');
+            res.redirect('/payment');
+        }
+    }
 }
