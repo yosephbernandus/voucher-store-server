620c80e805bff730d0d21c3d4026ffe3532ff44e
yosephbernandus <yosephbernandus@gmail.com>
2021-11-27 07:20:00 +0700

    add page payment

---
+++ app/payment/model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+let paymentScheme = mongoose.Schema({
+    type: {
+        type: String,
+        required: [true, 'Tipe Pembayaran harus diisi']
+    },
+    status: {
+        type: String,
+        enum: ['Y', 'N'],
+        default: 'Y'
+    },
+    banks: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Bank'
+    }]
+});
+
+module.exports = mongoose.model('Payment', paymentScheme);
