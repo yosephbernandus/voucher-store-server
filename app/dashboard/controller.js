2ed93d577260ce800592a3396d59f7278502bd63
yosephbernandus <yosephbernandus@gmail.com>
2021-11-28 14:35:38 +0700

    add dashboard page index

--- app/dashboard/controller.js
+++ app/dashboard/controller.js
@@ -1,7 +1,23 @@
+const Transaction = require('../transactions/model');
+const Voucher = require('../voucher/model');
+const Player = require('../player/model');
+const Category = require('../category/model');
+
 module.exports = {
     index: async (req, res) => {
         try {
-            res.render('index',{
+
+            const transaction = await Transaction.countDocuments()
+            const voucher = await Voucher.countDocuments()
+            const player = await Player.countDocuments()
+            const category = await Category.countDocuments()
+            res.render('admin/dashboard/view_dashboard',{
+                count: {
+                    transaction,
+                    voucher,
+                    player,
+                    category
+                },
                 name: req.session.user.name,
                 title: 'Halaman Dashboard'
             })
---
+++ app/player/model.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+let playerScheme = mongoose.Schema({
+    email: {
+        type: String,
+        required: [true, 'Email harus diisi']
+    },
+    name: {
+        type: String,
+        required: [true, 'nama harus diisi'],
+        maxlength: [225, 'panjang nama harus antara 9 - 225 karakter'],
+        minlength: [3, 'panjang nama harus antara 9 - 225 karakter']
+    },
+    username: {
+        type: String,
+        required: [true, 'username harus diisi'],
+        maxlength: [225, 'panjang username harus antara 9 - 225 karakter'],
+        minlength: [3, 'panjang username harus antara 9 - 225 karakter']
+    },
+    password: {
+        type: String,
+        required: [true, 'kata sandi harus diisi'],
+        maxlength: [225, 'panjang password harus 225 karakter'],
+    },
+    role: {
+        type: String,
+        enum: ['admin', 'user'],
+        default: 'user'
+    },
+    status: {
+        type: String,
+        enum: ['Y', 'N'],
+        default: 'Y'
+    },
+    avatar: {
+        type: String,
+    },
+    fileName: {
+        type: String,
+    },
+    phoneNumber: {
+        type: String,
+        required: [true, 'nomor telepon harus diisi'],
+        maxlength: [13, 'panjang nomor telepon harus antara 9 - 13 karakter'],
+        minlength: [9, 'panjang nomor telepon harus antara 9 - 13 karakter']
+    },
+    favorite: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+    },
+}, { timestamps: true });
+
+module.exports = mongoose.model('Player', playerScheme);
