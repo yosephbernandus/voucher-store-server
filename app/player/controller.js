c7f7514249795e271d87e9d60e11bc2685f0f139
yosephbernandus <yosephbernandus@gmail.com>
2021-11-29 23:09:17 +0700

    add api detail page

--- app/player/controller.js
+++ app/player/controller.js
@@ -9,7 +9,24 @@ module.exports = {
                 .populate('category');
             res.status(200).json({ data: voucher });
         } catch (err) {
-            res.status(500).json({message: err.message || 'Terjadi kesalahan apda server'});
+            res.status(500).json({message: err.message || 'Internal server error'});
         }
-    }
+    },
+
+    detailPage: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const voucher = await Voucher.findOne({ _id: id })
+                .populate('category')
+                .populate('nominals')
+                .populate('user', '_id name phoneNumber');
+
+            if (!voucher) {
+                return res.status(404).json({ message: 'Voucher not found' });
+            }
+            res.status(200).json({ data: voucher });
+        } catch (err) {
+            res.status(500).json({message: err.message || 'Internal server error'});
+        }
+    },
 }
