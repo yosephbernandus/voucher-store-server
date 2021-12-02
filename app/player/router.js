2b8d73185b729ad11a01be7436fee27dc75079fa
yosephbernandus <yosephbernandus@gmail.com>
2021-12-02 21:15:40 +0700

    add edit profile

--- app/player/router.js
+++ app/player/router.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const { dashboard, landingPage, detailPage, category, checkout, history, historyDetail, profile } = require('./controller');
+const { dashboard, landingPage, detailPage, category, checkout, history, historyDetail, profile, editProfile } = require('./controller');
 const { isLoginPlayer } = require('../middleware/auth');
+const multer = require('multer');
+const os = require('os');
 
 router.get('/landing-page', landingPage);
 router.get('/:id/detail', detailPage);
@@ -11,5 +13,6 @@ router.get('/history', isLoginPlayer, history);
 router.get('/history/:id/detail', isLoginPlayer, historyDetail);
 router.get('/dashboard', isLoginPlayer, dashboard);
 router.get('/profile', isLoginPlayer, profile);
+router.put('/profile', isLoginPlayer, multer({dest: os.tmpdir()}).single('image'), editProfile);
 
 module.exports = router;
