e615bd6f846f1237af385e4a20e1956d2e25fed0
yosephbernandus <yosephbernandus@gmail.com>
2021-12-05 12:10:39 +0700

    add payment bank

--- app/player/controller.js
+++ app/player/controller.js
@@ -29,10 +29,12 @@ module.exports = {
                 .populate('nominals')
                 .populate('user', '_id name phoneNumber');
 
+            const payment = await Payment.find().populate("banks");
             if (!voucher) {
                 return res.status(404).json({ message: 'Voucher not found' });
             }
-            res.status(200).json({ data: voucher });
+
+            res.status(200).json({ data: voucher, payment: payment });
         } catch (err) {
             res.status(500).json({message: err.message || 'Internal server error'});
         }
