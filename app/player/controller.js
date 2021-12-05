1bf9be83c7a8cff0fcd851c3111c95c0cc5ff0bf
yosephbernandus <yosephbernandus@gmail.com>
2021-12-05 12:20:22 +0700

    fix payment api return

--- app/player/controller.js
+++ app/player/controller.js
@@ -34,7 +34,11 @@ module.exports = {
                 return res.status(404).json({ message: 'Voucher not found' });
             }
 
-            res.status(200).json({ data: voucher, payment: payment });
+            data = {
+                'detail': voucher,
+                'payment': payment
+            }
+            res.status(200).json({ data: data });
         } catch (err) {
             res.status(500).json({message: err.message || 'Internal server error'});
         }
